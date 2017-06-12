ce20917b3d9277e0a7c5988ed095eb2a72203d09
zxclovetotti <zxclovetotti@gmail.com>
2017-06-12 20:06:27 +0800

    examples

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+    # Learning Topics
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # Return string
+        return self.text
