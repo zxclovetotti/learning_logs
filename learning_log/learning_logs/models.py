68e9104476d938c55bce75df694e5b95be0e29fa
zxclovetotti <zxclovetotti@gmail.com>
2017-06-13 21:05:05 +0800

    new features added

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -10,3 +10,13 @@ class Topic(models.Model):
     def __str__(self):
         # Return string
         return self.text
+
+class Entry(models.Model):
+    # knowledge about one topic
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # Return string
+        return self.text[:50] + "..."                                                                                                
