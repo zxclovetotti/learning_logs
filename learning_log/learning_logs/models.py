282ce7d0df3de5686a8b86293df11a2af3198f38
zxclovetotti <zxclovetotti@gmail.com>
2017-06-14 13:36:41 +0800

    example 18-2 completed

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -19,4 +19,7 @@ class Entry(models.Model):
 
     def __str__(self):
         # Return string
-        return self.text[:50] + "..."                                                                                              
+        if len(self.text) >= 50:
+            return self.text[:50] + "..."
+        else:
+            return self.text                                                                                   
