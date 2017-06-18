2cb3eb1d923a7a3a17002ab2078ab1c27dcb9f51
zxclovetotti <zxclovetotti@gmail.com>
2017-06-18 15:23:31 +0800

    exercise 18-8

--- learning_log/pizzeria/urls.py
+++ learning_log/pizzeria/urls.py
@@ -4,7 +4,14 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'pizzeria'
 urlpatterns = [
     # homepage
     url(r'^$', views.index, name='index'),
+
+    # pizzas
+    url(r'^/pizzas/$', views.pizzas, name='pizzas'),
+
+    # pizza
+    url(r'^/pizzas/(?P<pizza_id>d+)$', views.pizza, name='pizza'),
 ]
--- learning_log/pizzeria/views.py
+++ learning_log/pizzeria/views.py
@@ -3,6 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Pizza
+
 def index(request):
     # homepage of pizzeria
     return render(request, 'pizzeria/index.html')
+
+def pizzas(request):
+    # show all menu
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzeria/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    # show all details
+    pizza = Pizza.object.get(id=pizza_id)
+    toppings = pizza.toppings_set.order_by('-date_added')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzeria/pizza.html', context)
