d3917e633e6d48d3389b87b1ebe4c17c93c4968d
zxclovetotti <zxclovetotti@gmail.com>
2017-07-02 22:21:42 +0800

    19.3.6

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -44,7 +44,9 @@ def new_topic(request):
         # process submitted data
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     
     context = {'form': form}
